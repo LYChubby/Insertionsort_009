869d685b0291a17cfb7c3a1d1d8075605d85c5c2
LYChubby <f4uzi345@gmail.com>
2023-03-23 10:41:41 +0700

    Membuat Prosedur Input

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,20 +1,28 @@
-// InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int arr[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan Jumlah Data Pada Array : ";
+		cin >> n;
+
+		if (n <= 20) {     // Membuat Kondisi n Tidak Lebih Dari 20
+			break;
+		}
+		else {
+			cout << "\nArray Yang Anda Masukkan Maksimal 20 Elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) {      // Menggunakan Perulangan For Untuk Menyimpan Data Pada Array
+		cout << "Data Ke- " << (i + 1) << ": ";      // Memasukkan / Menginputkan Nilai Data n
+		cin >> arr[i];
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
